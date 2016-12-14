0abb034e9aa551510e158fa97018eafaf5fb6307
vogtn <nicolasjvogt@gmail.com>
2016-12-13 18:08:53 -0800

    adding changes

--- mernCommentBox/src/CommentList.js
+++ mernCommentBox/src/CommentList.js
@@ -7,7 +7,12 @@ class CommentList extends Component {
 	render() {
 		let commentNodes = this.props.data.map(comment => {
 			return(
-				<Comment author={ comment.author } key={ comment.id }>
+				<Comment
+					 author={ comment.author }
+					 uniqueId={comment['_id']}
+					 onCommentDelete={this.props.onCommentDelete}
+					 onCommentUpdate={this.props.onCommentUpdate}
+					 key={ comment['_id'] }>
 					{comment.text}
 				</Comment>
 			)
